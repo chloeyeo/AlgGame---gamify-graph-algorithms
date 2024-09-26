8409c6a3e7a1fa0b7004a129e0b9747587283689
Chloe Yeo <yeochloe01@gmail.com>
2024-09-26 21:03:23 +0100

    modified for export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Enabling static HTML export
+  output: "export",
+  // Optional: you can enable trailing slashes in URLs
+  trailingSlash: true,
+};
 
 export default nextConfig;
