d47e57a946f749508c74d53e13a8dad23b498977
chloeyeo <yeochloe01@gmail.com>
2025-02-09 13:38:25 +0000

    pseudocode highlight update

--- src/components/CodeEditorPseudocode.js
+++ src/components/CodeEditorPseudocode.js
@@ -4,18 +4,16 @@ const CodeEditorPseudocode = ({ pseudocode, highlightedLines = [] }) => {
   const lines = pseudocode.split("\n");
 
   return (
-    <pre className="bg-black text-white p-4 rounded-lg overflow-x-auto">
+    <pre className="bg-black text-white p-4 rounded-lg overflow-x-auto relative">
       {lines.map((line, index) => (
-        <div
-          key={index}
-          className={`font-mono transition-colors duration-200 w-full ${
-            highlightedLines.includes(index + 1)
-              ? "bg-yellow-500 bg-opacity-50"
-              : ""
-          } px-2 -mx-2`}
-        >
-          <span className="select-none text-gray-500 mr-4">{index + 1}</span>
-          {line}
+        <div key={index} className="relative">
+          {highlightedLines.includes(index + 1) && (
+            <div className="absolute inset-y-0 -left-4 -right-4 bg-yellow-500 bg-opacity-50" />
+          )}
+          <div className="relative font-mono w-max min-w-full px-2">
+            <span className="select-none text-gray-500 mr-4">{index + 1}</span>
+            {line}
+          </div>
         </div>
       ))}
     </pre>
