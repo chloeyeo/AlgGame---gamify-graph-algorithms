36bb277d97cd40446692ef04d5b8660eda043ec5
chloeyeo <yeochloe01@gmail.com>
2025-02-11 01:29:48 +0000

    leaderboard update

--- src/components/leaderboard/Leaderboard.js
+++ src/components/leaderboard/Leaderboard.js
@@ -125,7 +125,7 @@ const Leaderboard = ({ algorithm }) => {
       </div>
 
       <div className="overflow-x-auto no-scrollbar">
-        <table className="min-w-full">
+        <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
               <th className="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase w-16">
